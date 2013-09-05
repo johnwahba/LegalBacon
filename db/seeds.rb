d383acb8097c99ec7ec720bb4517df6745c3f829
John Wahba <john.a.wahba@gmail.com>
2013-09-05 16:49:50 -0700

    Switch to MYSQL

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# p system('rails runner script/default_info.rb')
+p system('rails runner script/default_info.rb')
 # ScotusCase.destroy_all
-# p system('rails runner script/add_cases_to_db.rb ../US')
+p system('rails runner script/add_cases_to_db.rb ../US')
 p system('rails runner script/clean_cases_db.rb')
