e98db1caaa1cff8db536b97f918bae4ce26c0b9c
John Wahba <john.a.wahba@gmail.com>
2013-09-12 13:30:41 -0700

    fix linkifier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 	JUSTICE_NAME_REGEX_2 = /[A-Z \.]{2,100} ([^(?:II)](?:Mc|VAN |O')?[A-Z]{2,100})(?:, Jr\.| II)?$/i
 	NAME_REGEX = /(?:Mc|VAN |O')?[A-Z]{2,100}/
-	SCOTUS_CITATION_REGEX= /[0-9]{1,4} U\. ?S\. [0-9]{1,4}/
+	SCOTUS_CITATION_REGEX= /([24[5-9]|2[5-9][0-9]|[3-9][0-9]{2}] U\. ?S\. [0-9]{1,4})/
+	SCOTUS_CITATION_CAPTURE= /([24[5-9]|2[5-9][0-9]|[3-9][0-9]{2})] U\. ?S\. ([0-9]{1,4})/
 	OPINION_TYPE_DETECTION_REGEX= /concur|dissent|PER CURIAM|deliver|memo|no part/i
 
 	def paragraph_author(paragraph)
@@ -11,6 +12,20 @@ module ApplicationHelper
 			paragraph.paragraph_index_number
 		end
 	end
+
+	def linkify(paragraph)
+		split_paragraph= paragraph.split(SCOTUS_CITATION_REGEX)
+		split_paragraph.map! do |fragment|
+			if fragment =~ SCOTUS_CITATION_REGEX
+				"<a href='#{case_url(*fragment.scan(SCOTUS_CITATION_CAPTURE)[0])}'> #{fragment}</a>"
+			else
+				fragment
+			end
+		end
+		split_paragraph.join('')
+
+	end
+
 
 				
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LegalDatabase::Application.routes.draw do
 
   get "cases/", to: 'volumes#index'
-  get "cases/:volume_number/US/:start_page(/:opinion)", to: 'scotus_cases#show', as: "case"
+  get "cases/:volume_number/US/:start_page", to: 'scotus_cases#show', as: "case"
   get "cases/:volume_number/US/", to: 'volumes#show', as: "volume"
   get "justice/:justice_id", to: 'justice#show', as: "justice"
   get "justices(/:sorter)", to: "justice#index", as: "justices"
