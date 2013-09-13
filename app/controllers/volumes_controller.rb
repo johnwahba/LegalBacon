dc8e49ea45e715c99737966f86a13127d7a530c1
John Wahba <john.a.wahba@gmail.com>
2013-09-13 14:06:46 -0700

    commit for push

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -1,6 +1,7 @@
 class VolumesController < ApplicationController
   def show
-  	@cases = ScotusCase.where(volume: params[:volume_number])
+  	@cases = ScotusCase.where({volume: params[:volume_number]})
+  	p params[:volume_number]
   end
 
   def index
