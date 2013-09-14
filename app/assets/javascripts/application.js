b98366647a4403d72c64c98da925763b4a508d43
John Wahba <john.a.wahba@gmail.com>
2013-09-14 05:49:25 -0700

    nuked typeahead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//= require typeahead
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
